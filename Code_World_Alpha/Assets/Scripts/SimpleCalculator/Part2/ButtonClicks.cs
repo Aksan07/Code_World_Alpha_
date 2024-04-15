c972557aa4cda56753e6b7b0002a3123517af5ff
Aksan Pramod <116940380+Aksan07@users.noreply.github.com>
2024-04-16 01:20:05 +0530

    16:9

--- Code_World_Alpha/Assets/Scripts/SimpleCalculator/Part2/ButtonClicks.cs
+++ Code_World_Alpha/Assets/Scripts/SimpleCalculator/Part2/ButtonClicks.cs
@@ -1,17 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class ButtonClicks : MonoBehaviour
 {
     [SerializeField] private AudioSource _source;
     [SerializeField] private AudioClip _errorSoundClip,_successSoundClip;
+    [SerializeField] GameObject printobj,buttonobj,correctbutton;
+    [SerializeField]Text question;
+    
 public void buttonClickcorrect(){
     Debug.Log("button has been clicked");
     _source.PlayOneShot(_successSoundClip);
+    question.text="OK Now I remember";
+    //correctbutton.GetComponent<Animator>().enabled = true;
+    printobj.GetComponent<Animator>().enabled=true;
+    buttonobj.GetComponent<Animator>().enabled=true;
+    correctbutton.GetComponent<Animator>().enabled=true;
+    StartCoroutine(destroyobj(buttonobj));
+    StartCoroutine(destroyobj(correctbutton));
+    
+
+    
 }
 public void buttonClickwrong(){
     Debug.Log("wrong");
-    _source.PlayOneShot(_successSoundClip);
+    _source.PlayOneShot(_errorSoundClip);
+}
+IEnumerator destroyobj(GameObject gameObject){
+    yield return new WaitForSeconds(5f);
+    Destroy(gameObject);
 }
 }
