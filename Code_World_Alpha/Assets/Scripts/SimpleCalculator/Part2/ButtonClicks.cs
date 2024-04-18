1a4a8d1891f6a40a9e9ca6cb5c0a2c1ed3ab9d1f
Aksan Pramod <116940380+Aksan07@users.noreply.github.com>
2024-04-18 12:34:09 +0530

    SimpleCalculator 80%

--- Code_World_Alpha/Assets/Scripts/SimpleCalculator/Part2/ButtonClicks.cs
+++ Code_World_Alpha/Assets/Scripts/SimpleCalculator/Part2/ButtonClicks.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine;
 
@@ -9,17 +9,19 @@ public class ButtonClicks : MonoBehaviour
     [SerializeField] private AudioClip _errorSoundClip,_successSoundClip;
     [SerializeField] GameObject printobj,buttonobj,correctbutton;
     [SerializeField]Text question;
+    [SerializeField] string sceneName;
     
 public void buttonClickcorrect(){
     Debug.Log("button has been clicked");
     _source.PlayOneShot(_successSoundClip);
-    question.text="oo yess ";
+    question.text="Okieee...Now I Remember ";
     //correctbutton.GetComponent<Animator>().enabled = true;
     printobj.GetComponent<Animator>().enabled=true;
     buttonobj.GetComponent<Animator>().enabled=true;
     correctbutton.GetComponent<Animator>().enabled=true;
     StartCoroutine(destroyobj(buttonobj));
     StartCoroutine(destroyobj(correctbutton));
+    GoToScene(sceneName);
 }
 public void buttonClickwrong(){
     Debug.Log("wrong");
@@ -29,4 +31,12 @@ IEnumerator destroyobj(GameObject gameObject){
     yield return new WaitForSeconds(5f);
     Destroy(gameObject);
 }
+    public void GoToScene(string sceneName){
+       StartCoroutine(LoadAsynchronously(sceneName));
+    }
+    IEnumerator LoadAsynchronously(string sceneName){
+        yield return new WaitForSeconds(2f);
+         AsyncOperation operations =SceneManager.LoadSceneAsync(sceneName);
+            
+         }
 }
--- Code_World_Alpha/Assets/Scripts/SimpleCalculator/Part3/P3PuzzleManager.cs
+++ Code_World_Alpha/Assets/Scripts/SimpleCalculator/Part3/P3PuzzleManager.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class P3PuzzleManager : MonoBehaviour
 {
 [SerializeField] int totalPuzzlePieces; // Set the total number of puzzle pieces here
     private int solvedPuzzlePieces = 0;
+    [SerializeField]Text dialogue;
+    [SerializeField] string sceneName;
+    [SerializeField] private AudioSource _source;
+    [SerializeField] private AudioClip _successSoundClip;
+
     public void PieceSolved()
     {
         solvedPuzzlePieces++;
 
         if (solvedPuzzlePieces >= totalPuzzlePieces)
         {
-            Debug.Log("Puzzles Completed");
+            dialogue.enabled=false;
+           _source.PlayOneShot(_successSoundClip);
+
+           GoToScene(sceneName);
         }
     }
+    public void GoToScene(string sceneName){
+       StartCoroutine(LoadAsynchronously(sceneName));
+    }
+    IEnumerator LoadAsynchronously(string sceneName){
+        yield return new WaitForSeconds(2f);
+         AsyncOperation operations =SceneManager.LoadSceneAsync(sceneName);
+            
+         }
 }
